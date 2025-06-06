f3eed31070daaa58a4e445cf002cba083554f34f
Anit Paul <anit42655@gmail.com>
2025-06-06 10:06:04 +0530

    string.js added

---
+++ string.js
@@ -0,0 +1,8 @@
+const name="anit"
+const middle="kumar"
+const last="paul"
+console.log(`my name is ${name} ${middle} ${last}`);
+console.log(name.indexOf('i'));
+console.log(`my name is ${name} ${middle} ${last}`.split(" "));
+console.log(`my name is ${name} ${middle} ${last}`.replace("paul","Paul"))
+
