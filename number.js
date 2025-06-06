2d5f6aebc5b597b9ec40c519796e57e4fa7ccd1a
Anit Paul <anit42655@gmail.com>
2025-06-06 10:22:12 +0530

    number.js added

---
+++ number.js
@@ -0,0 +1,13 @@
+number=11.56
+console.log(number.toPrecision(3)); //take 3 value from the front and round off that
+const num=1000000
+console.log(num.toLocaleString('en-in'));
+
+//++++++++++++++++++++++maths+++++++++++++++++++++++
+console.log(Math.pow(3,2));
+console.log(((Math.random()*10)+1))
+//get random  between 10-20
+max=20
+min=10
+console.log(Math.floor(Math.random()*(max-min))+min)
+
